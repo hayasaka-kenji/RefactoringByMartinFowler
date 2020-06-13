b5cedbc685db14108f38bd8f04576aa91f630177
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-14 01:18:11 +0900

    関数の独立

--- practice/chapter01/main.js
+++ practice/chapter01/main.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
+const { create } = require('domain');
 const invoice = JSON.parse(fs.readFileSync(__dirname + '/json/invoices.json', 'utf8'));
 const plays = JSON.parse(fs.readFileSync(__dirname + '/json/plays.json', 'utf8'));
 
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return statementData;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -51,23 +56,17 @@ function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result = perf.volumeCredits;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
