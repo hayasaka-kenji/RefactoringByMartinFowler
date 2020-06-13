4dfe595c6aef7e5bd5e875d255c242437a141813
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-13 20:29:27 +0900

    ループの分離。

--- practice/chapter01/main.js
+++ practice/chapter01/main.js
@@ -4,16 +4,15 @@ const plays = JSON.parse(fs.readFileSync(__dirname + '/json/plays.json', 'utf8')
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
-
-    volumeCredits = volumeCreditsFor(perf);
-
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
+  }
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits = volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
