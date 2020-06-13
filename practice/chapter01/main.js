d0758cf74659d6f628302c523fa9417b48af7912
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-13 19:53:50 +0900

    変数の置き換え

--- practice/chapter01/main.js
+++ practice/chapter01/main.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const invoice = JSON.parse(fs.readFileSync(__dirname + '/json/invoices.json', 'utf8'));
 const plays = JSON.parse(fs.readFileSync(__dirname + '/json/plays.json', 'utf8'));
 
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -11,7 +15,7 @@ function statement (invoice, plays) {
       minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf); // 一時置き換え
     let thisAmount = amountFor(perf, play);
 
     // ボリューム特典のポイントを加算
@@ -29,21 +33,21 @@ function statement (invoice, plays) {
 }
 
 
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
         throw new Error(`unknown type: ${play.type}`);
