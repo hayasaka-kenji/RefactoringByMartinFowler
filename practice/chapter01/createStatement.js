cab7d85747fecad8fc7d7cf6db243e9c52c4e2d2
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-14 03:25:17 +0900

    関数をPerformanceCalculatorに集約

--- practice/chapter01/createStatement.js
+++ practice/chapter01/createStatement.js
@@ -7,18 +7,48 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    // 一時的にclass定義をここに追加。
     class PerformanceCalculator {
       constructor(aPerformance, aPlay) {
         this.performances = aPerformance;
         this.play = aPlay;
+      }
+
+      get amount() {
+        let result = 0;
+        switch (this.play.type) {
+          case "tragedy":
+            result = 40000;
+            if (this.performances.audience > 30) {
+              result += 1000 * (this.performances.audience - 30);
+            }
+            break;
+          case "comedy":
+            result = 30000;
+            if (this.performances.audience > 20) {
+              result += 10000 + 500 * (this.performances.audience - 20);
+            }
+            result += 300 * this.performances.audience;
+            break;
+          default:
+            throw new Error(`unknown type: ${this.play.play.type}`);
+        }
+        return result;
+      }
+
+      get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performances.audience - 30, 0);
+        if ("comedy" === this.play.type) result += Math.floor(this.performances.audience / 5);
+        return result;
       }
     }
 
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -27,31 +57,11 @@ function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function totalAmount(data) {
