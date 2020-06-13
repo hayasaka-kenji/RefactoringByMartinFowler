deb3b6ced30ac767708f9021e99c069df7f20d2a
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-13 20:04:57 +0900

    変数のインライン化

--- practice/chapter01/main.js
+++ practice/chapter01/main.js
@@ -15,7 +15,6 @@ function statement (invoice, plays) {
       minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -23,8 +22,8 @@ function statement (invoice, plays) {
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
