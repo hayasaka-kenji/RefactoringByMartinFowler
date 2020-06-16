a5dd42220c1aae381f3a891e60e1812ca3b5a6d7
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-16 21:12:27 +0900

    テストツールの比較

---
+++ test/calc.js
@@ -0,0 +1,11 @@
+module.exports.add = function (a,b) {
+  return a + b;
+};
+
+module.exports.square = function (a) {
+  return a * a;
+};
+
+module.exports.cube = function (a) {
+  return a * a * a;
+};
---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var myfuncs = require('./calc.js');
+
+describe('myfuncs', function() {
+  describe('add', function() {
+    it ('第１引数と第２引数を足した結果を返す', function() {
+      assert.equal(myfuncs.add(1,2), 3);
+    });
+  });
+  describe('square', function() {
+    it ('２乗した結果を返す', function() {
+      assert.equal(myfuncs.square(2), 4);
+    });
+  });
+  describe('cube', function() {
+    it ('３乗した結果を返す', function() {
+      assert.equal(myfuncs.cube(2), 8);
+    });
+  });
+});
