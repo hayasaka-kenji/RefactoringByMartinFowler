3d73108c7ad18b4cf2b730d3ca097b8ecc9cf738
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-22 20:47:47 +0900

    sampleにexpectを追加。

--- src/sample.js
+++ src/sample.js
@@ -4,4 +4,8 @@ function add(a, b) {
   return a + b;
 };
 
-export { add };
+function sub(a, b) {
+  return a - b;
+};
+
+export { add, sub };
--- test/sample.test.js
+++ test/sample.test.js
@@ -1,9 +1,14 @@
 'use strict';
-import { assert } from "chai";
-import { add } from "../src/sample";
+import { expect, assert } from "chai";
+import { add, sub } from "../src/sample";
 
 describe('Sample', () => {
   it('add', () => {
     assert.equal(add(1, 2), 3);
   });
+
+  it('sub', () => {
+    expect(sub(2, 1)).equal(1);
+  });
 });
+
