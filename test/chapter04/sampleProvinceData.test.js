1ab063aa36098a0e3e181789f4a2540d6c6c60bc
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-22 16:53:55 +0900

    フィクスチャの変更

--- test/chapter04/sampleProvinceData.test.js
+++ test/chapter04/sampleProvinceData.test.js
@@ -15,4 +15,10 @@ describe('chapter04.province', function () {
   it('profit', function () {
     expect(asia.profit).equal(230);
   });
+
+  it('change a production', function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).equal(-6);
+    expect(asia.profit).equal(292);
+  })
 });
