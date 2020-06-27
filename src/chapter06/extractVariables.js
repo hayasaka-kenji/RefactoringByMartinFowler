0ca3a30a4dbddca815bddc2492b643287391883e
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-27 20:54:09 +0900

    変数を定義して置き換える

--- src/chapter06/extractVariables.js
+++ src/chapter06/extractVariables.js
@@ -1,7 +1,10 @@
 export { price };
 
 function price(order) {
-  return order.quantity * order.itemPrice -
+  // 価格 = 本体価格 - 数量　+ 単価
+  // price = base prise -  quantity discount + shipping
+  const basePrice = order.quantity * order.itemPrice;
+  return basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
     Math.min(order.quantity) * order.itemPrice * 0.01;
 }
