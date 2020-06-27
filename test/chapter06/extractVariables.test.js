6b2f71e8cd5b27e90a39808c38d81c9409ac532d
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-27 20:49:30 +0900

    関数の配置

---
+++ test/chapter06/extractVariables.test.js
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import { price } from "../../src/chapter06/extractVariables";
+
+describe('chapter06: extractVariables', () => {
+  it('If defined as {quantity: 50, temPrice: 200}, returns 10100', () => {
+    const order = {
+      quantity: 50,
+      itemPrice: 200
+    };
+    expect(price(order)).equal(10100);
+  });
+});
+
