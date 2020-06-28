9b73cc1317f5b69cfc2b44e91c3c160fe3a218d5
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-28 20:33:58 +0900

    中間データ構造を返す関数を作成。関数内のreturnを整える。

--- src/chapter06/splitPhase.js
+++ src/chapter06/splitPhase.js
@@ -1,16 +1,18 @@
 export function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
-  const priceData = { basePrice: basePrice, quantity: quantity, discount: discount };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice: basePrice, quantity: quantity, discount: discount };
 }
 
-export function applyShipping(priceData, shippingMethod) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - priceData.discount + shippingCost;
-  return price;
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
