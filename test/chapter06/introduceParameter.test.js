ecc49ab456e019e96a6d4bb6f18b5537bfe682bd
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-28 16:55:08 +0900

    intoroduce prameter object : 初期ファイルの配置

---
+++ test/chapter06/introduceParameter.test.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import { readingsOutsideRange } from "../../src/chapter06/introduceParameter";
+
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ]
+};
+
+const operationPlan = {
+  temperatureFloor: 48,
+  temperatureCeiling: 53
+};
+
+describe('readingOutsideRange', () => {
+  let alerts = readingsOutsideRange(station,
+                                    operationPlan.temperatureFloor,
+                                    operationPlan.temperatureCeiling);
+  let result = [{ temp: 47, time: '2016-11-10 09:10' },
+  { temp: 58, time: '2016-11-10 09:30' }];
+  it('Test out of temperature range', () => {
+    expect(alerts).to.eql(result);
+  });
+});
