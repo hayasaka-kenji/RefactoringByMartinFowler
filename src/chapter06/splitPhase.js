fce7d00d6a3fc8b1379a7f55544c532725af9cd3
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-28 20:27:35 +0900

    中間データ構造の作成。

--- src/chapter06/splitPhase.js
+++ src/chapter06/splitPhase.js
@@ -2,15 +2,15 @@ export function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
-  const priceData = {};
-  const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
+  const priceData = { basePrice: basePrice, quantity: quantity, discount: discount };
+  const price = applyShipping(priceData, shippingMethod);
   return price;
 }
 
-export function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
-  const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-    ? shippingMethod.discountedFee: shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+export function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+    ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
