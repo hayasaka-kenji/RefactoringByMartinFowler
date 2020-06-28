58e07f9beb0f5c17bbed5d050a352c9ed19dc7a9
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-06-28 10:52:07 +0900

    変数のカプセル化

---
+++ test/chapter06/encapsulateVariable1.test.js
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { getDefaultOwner, setDefaultOwner } from "../../src/chapter06/encapsulateVariable1";
+
+describe('defaultOwner', () => {
+  it('Whether Martin Folwer', () => {
+    let spaceship = {};
+    spaceship.owner = getDefaultOwner();
+
+    expect(spaceship.owner.firstName).equal('Martin');
+    expect(spaceship.owner.lastName).equal('Fowler');
+  });
+
+  it('Make a change', () => {
+    setDefaultOwner({firstName: 'Rebecca', lastName: 'Persons'});
+    let user = getDefaultOwner();
+
+    expect(user.firstName).equal('Rebecca');
+    expect(user.lastName).equal('Persons');
+  });
+});
