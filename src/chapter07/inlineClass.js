5377190aba233e1f807ca951caa3b1b04ffb7442
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-04 18:21:54 +0900

    Shipmentに移行が完了。

--- src/chapter07/inlineClass.js
+++ src/chapter07/inlineClass.js
@@ -1,4 +1,20 @@
-export class TrackingInformation {
+// export class TrackingInformation {
+//   constructor(company, number) {
+//     this._shippingCompany = company;
+//     this._trackingNumber = number;
+//   }
+
+//   get shippingCompany() { return this._shippingCompany; }
+//   set shippingCompany(arg) { this._shippingCompany = arg; }
+//   get trackingNumber() { return this._trackingNumber; }
+//   set trackingNumber(arg) { this._trackingNumber = arg; }
+//   get display() {
+//     return `${this.shippingCompany}: ${this.trackingNumber}`;
+//   }
+// }
+
+// TrackingInformationはShipmentの中で抽出される。
+export class Shipment {
   constructor(company, number) {
     this._shippingCompany = company;
     this._trackingNumber = number;
@@ -8,24 +24,9 @@ export class TrackingInformation {
   set shippingCompany(arg) { this._shippingCompany = arg; }
   get trackingNumber() { return this._trackingNumber; }
   set trackingNumber(arg) { this._trackingNumber = arg; }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
-// TrackingInformationはShipmentの中で抽出される。
-export class Shipment {
-  constructor(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation;
-  }
 
   get trackingInfo() {
-    return this._trackingInformation.display;
-  }
-
-  get trackingInformation() {return this._trackingInformation;}
-
-  set shippingCompany(aShippingCompany) {
-    this._trackingInformation.shippingCompany = aShippingCompany;
+    // return this._trackingInformation.display;
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
--- test/chapter07/inlineClass.test.js
+++ test/chapter07/inlineClass.test.js
@@ -1,18 +1,18 @@
 import { expect } from 'chai';
-import { TrackingInformation, Shipment } from '../../src/chapter07/inlineClass';
+import { Shipment } from '../../src/chapter07/inlineClass';
 
-describe('TrackingInformation', () => {
-  it('Enter number and company', () => {
-    const anInfo = new TrackingInformation('ART', '0120');
-    expect(anInfo.shippingCompany).equal('ART');
-    expect(anInfo.trackingNumber).equal('0120');
-    expect(anInfo.display).equal('ART: 0120');
-  });
-});
+// describe('TrackingInformation', () => {
+//   it('Enter number and company', () => {
+//     const anInfo = new TrackingInformation('ART', '0120');
+//     expect(anInfo.shippingCompany).equal('ART');
+//     expect(anInfo.trackingNumber).equal('0120');
+//     expect(anInfo.display).equal('ART: 0120');
+//   });
+// });
 
 describe('Shipment', () => {
   it('Enter name and number using Shipment', () => {
-    const aShipment = new Shipment(new TrackingInformation('ART', '0120'));
+    const aShipment = new Shipment('ART', '0120');
     expect(aShipment.trackingInfo).to.eql('ART: 0120');
 
     aShipment.shippingCompany = 'SAKAI';
