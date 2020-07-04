8d61311fb345bfcbf794dd62b51a918c41d1300c
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-04 20:59:42 +0900

    ディレクトリがなかった時の処理を追加。

--- tools/touchScript.sh
+++ tools/touchScript.sh
@@ -3,8 +3,18 @@
 # Usage:
 # sh tools/touchScript.sh 08 hogeHoge
 
-SCRIPT_FILE="src/chapter$1/$2.js"
-TEST_FILE="test/chapter$1/$2.test.js"
+SCRIPT_DIR="src/chapter$1"
+TEST_DIR="test/chapter$1"
+SCRIPT_FILE="$SCRIPT_DIR/$2.js"
+TEST_FILE="$TEST_DIR/$2.test.js"
+
+if [ ! -e $SCRIPT_DIR ]; then
+  mkdir $SCRIPT_DIR
+fi
+
+if [ ! -e $TEST_DIR ]; then
+  mkdir $TEST_DIR
+fi
 
 touch $SCRIPT_FILE
 cp test/template.test.js $TEST_FILE
