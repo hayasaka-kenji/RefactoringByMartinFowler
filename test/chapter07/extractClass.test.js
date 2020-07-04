b1c4b993c07e955749ad60ef6b811d1195d5e667
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-04 16:54:57 +0900

    テストの結果を細かくわかるように修正。

--- test/chapter07/extractClass.test.js
+++ test/chapter07/extractClass.test.js
@@ -2,11 +2,24 @@ import { expect } from 'chai';
 import { Person } from '../../src/chapter07/extractClass';
 
 describe('Person', () => {
-  it('Is the name, area code, number, phone number.', () => {
-    const aPerson = new Person('Paul', '0120', '333333');
+  let aPerson;
+  beforeEach(() => {
+    aPerson = new Person('Paul', '0120', '333333');
+  });
+
+  it('name', () => {
     expect(aPerson.name).equal('Paul');
+  });
+
+  it('area code', () => {
     expect(aPerson.officeAreaCode).equal('0120');
+  });
+
+  it('number', () => {
     expect(aPerson.officeNumber).equal('333333');
+  });
+
+  it('telephone number', () => {
     expect(aPerson.telephoneNumber).equal('(0120) 333333');
   });
 });
