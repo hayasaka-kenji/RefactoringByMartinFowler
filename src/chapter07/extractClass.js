5c33e423d1d6ceaa739656ca66f019ea0a30518c
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-04 17:10:08 +0900

    子クラスの作成とフィールドの移動。

--- src/chapter07/extractClass.js
+++ src/chapter07/extractClass.js
@@ -3,13 +3,36 @@ export class Person {
     this._name = name;
     this._officeAreaCode = areaCode;
     this._officeNumber = number;
+    this._telephoneNumber = new TelephoneNumber(areaCode, number);
   }
 
   get name() { return this._name; }
+
   set name(arg) { this._name = arg; }
-  get telephoneNumber() { return `(${this._officeAreaCode}) ${this._officeNumber}`; }
+
+  get telephoneNumber() { 
+    return `(${this._officeAreaCode}) ${this._officeNumber}`;
+  }
+
+  get officeAreaCode() { 
+    return this._telephoneNumber.officeAreaCode; 
+  }
+
+  set officeAreaCode(arg) { 
+    this._telephoneNumber._officeAreaCode = arg;
+  }
+
+  get officeNumber() { return this._officeNumber; }
+
+  set officeNumber(arg) { this._officeNumber = arg; }
+}
+
+class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._officeAreaCode = areaCode;
+    this._officeNumber = number;
+  }
+
   get officeAreaCode() { return this._officeAreaCode; }
   set officeAreaCode(arg) { this._officeAreaCode = arg; }
-  get officeNumber() { return this._officeNumber; }
-  set officeNumber(arg) { this._officeNumber = arg; }
 }
