a97f5fae6e6b07583df9b35c49f73c1948f7d7af
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 16:17:37 +0900

    ループとアルゴリズムの置き換え

--- src/chapter08/splitLoop.js
+++ src/chapter08/splitLoop.js
@@ -1,19 +1,11 @@
 // 給与totalSalaryと最年少youngestを求める
 export function report(people) {
   function totalSalary() {
-    let totalSalary = 0;
-    for (const p of people) {
-      totalSalary += p.salary;
-    }
-    return totalSalary;
+    return people.reduce((total, p) => total + p.salary, 0);
   }
 
   function youngest() {
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-      if (p.age < youngest) youngest = p.age;
-    }
-    return youngest;
+    return Math.min(...people.map(p => p.age));
   }
 
   return `youngest age: ${youngest()}, total salary: ${totalSalary()}`;
