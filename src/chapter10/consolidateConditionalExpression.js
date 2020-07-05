7ce226404be59b5f3acc31763a1c90d68916846c
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 22:42:24 +0900

    関数の抽出。

--- src/chapter10/consolidateConditionalExpression.js
+++ src/chapter10/consolidateConditionalExpression.js
@@ -1,7 +1,11 @@
 // 障害手当を計算する
 export function disabilityAmount(anEmployee) {
-  if ((anEmployee.seniority < 2)
-      || (anEmployee.monthsDisabled > 12)
-      || (anEmployee.isPartTime)) return 0;
-  return 2;
+  if (isNotEligibkeForDisability()) return 0;
+  return 2; // 手当があった場合
+
+  function isNotEligibkeForDisability() {
+    return ((anEmployee.seniority < 2)
+        || (anEmployee.monthsDisabled > 12)
+        || (anEmployee.isPartTime));
+  }
 }
