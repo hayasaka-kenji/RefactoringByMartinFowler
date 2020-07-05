7c4f846021ba28792f183c9fd8c9653572b404ed
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 17:40:11 +0900

    パイプラインの記述。

--- src/chapter08/replaceLoopwithPipeline.js
+++ src/chapter08/replaceLoopwithPipeline.js
@@ -1,18 +1,16 @@
 // インドの都市を選んで都市名と電話番号を返す。
 export function acquireData(input) {
   const lines = input.split("\n");
-  let firstLine = true;
   const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === "") continue;
-    const record = line.split(",");
-    if (record[1].trim() === "India") {
-      result.push({city: record[0].trim(), phone: record[2].trim()});
-    }
+  const loopItems = lines
+          .slice(1) // 1行目を削除
+          .filter(line => line.trim() !== '') // 空白行の処理
+          .map(line => line.split(','))
+          .filter(record => record[1].trim() === "India")
+          ;
+  for (const line of loopItems) {
+    const record = line;
+    result.push({city: record[0].trim(), phone: record[2].trim()});
   }
   return result;
 }
