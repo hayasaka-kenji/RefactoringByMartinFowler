e7463c4b38eb25716acd024b60c141e3c410a51a
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 16:08:52 +0900

    ループの分離。

--- src/chapter08/splitLoop.js
+++ src/chapter08/splitLoop.js
@@ -1,9 +1,13 @@
 // 給与totalSalaryと最年少youngestを求める
 export function report(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
   let totalSalary = 0;
+  let youngest = people[0] ? people[0].age : Infinity;
+
   for (const p of people) {
     if (p.age < youngest) youngest = p.age;
+  }
+
+  for (const p of people) {
     totalSalary += p.salary;
   }
 
