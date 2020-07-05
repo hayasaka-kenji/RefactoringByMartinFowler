9cfcfcacb475866a37fea9ce80a3837ab39dc492
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 16:06:46 +0900

    テストの項目を追加。

--- test/chapter08/splitLoop.test.js
+++ test/chapter08/splitLoop.test.js
@@ -2,22 +2,27 @@ import { expect } from 'chai';
 import { report } from '../../src/chapter08/splitLoop.js';
 
 describe('report', () => {
-  const data = [
-    {
-      age: 30,
-      salary: 30
-    },
-    {
-      age: 67,
-      salary: 100
-    },
-    {
-        age: 42,
-        salary: 50
-    }
-  ];
 
   it('Test the report with data', () => {
-    expect(report(data)).equal('youngest: 30, totalSalary: 180');
+    const aPeople = [
+      {
+        age: 30,
+        salary: 30
+      },
+      {
+        age: 67,
+        salary: 100
+      },
+      {
+          age: 42,
+          salary: 50
+      }
+    ];
+    expect(report(aPeople)).equal('youngest: 30, totalSalary: 180');
+  });
+
+  it('Test if it is empty data', () => {
+    const aPeople = [];
+    expect(report(aPeople)).equal('youngest: Infinity, totalSalary: 0');
   });
 });
