abdf01f8e9cef07f2f931f0d36749fdabc51286e
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 10:01:04 +0900

    テストの作成

--- test/chapter08/moveFunction.test.js
+++ test/chapter08/moveFunction.test.js
@@ -1,9 +1,22 @@
 import { expect } from 'chai';
-import { add } from '../../src/chapter';
+import { trackSummary } from '../../src/chapter08/moveFunction'
 
-describe('Sample', () => {
-  it('sub', () => {
-    expect(sub(2, 1)).equal(1);
+describe('trackSummary', () => {
+  it('calculate distance', () => {
+    const newYork = {
+      lat: 40.73061,
+      lon: -73.935242
+    };
+
+    const tokyo = {
+      lat: 35.652832,
+      lon: 139.839478
+    };
+
+    expect(trackSummary([newYork, tokyo])).to.eql({
+      time: 10000,
+      distance: 6740.914927144901,
+      pace: 0.02472463581991205,
+    });
   });
 });
-
