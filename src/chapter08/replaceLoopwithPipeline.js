9104419a5a8ed72db600472ef0c08afb34bcc9be
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 17:44:50 +0900

    ループを削除して処理結果のみにする。

--- src/chapter08/replaceLoopwithPipeline.js
+++ src/chapter08/replaceLoopwithPipeline.js
@@ -1,16 +1,12 @@
 // インドの都市を選んで都市名と電話番号を返す。
 export function acquireData(input) {
   const lines = input.split("\n");
-  const result = [];
-  const loopItems = lines
+  const result = lines
           .slice(1) // 1行目を削除
           .filter(line => line.trim() !== '') // 空白行の処理
-          .map(line => line.split(','))
-          .filter(record => record[1].trim() === "India")
+          .map(line => line.split(',')) // カンマの削除
+          .filter(record => record[1].trim() === "India") // 指定したものだけ取り出す。
+          .map(record => ({city: record[0].trim(), phone: record[2].trim()})) // 出力の形式に設定。
           ;
-  for (const line of loopItems) {
-    const record = line;
-    result.push({city: record[0].trim(), phone: record[2].trim()});
-  }
   return result;
 }
