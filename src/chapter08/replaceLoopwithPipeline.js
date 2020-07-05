76b62f7384a08077a5c3536437934491ad6b7b83
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 17:48:17 +0900

    変数を適宜変更。コメントを削除。

--- src/chapter08/replaceLoopwithPipeline.js
+++ src/chapter08/replaceLoopwithPipeline.js
@@ -1,12 +1,11 @@
 // インドの都市を選んで都市名と電話番号を返す。
 export function acquireData(input) {
   const lines = input.split("\n");
-  const result = lines
-          .slice(1) // 1行目を削除
-          .filter(line => line.trim() !== '') // 空白行の処理
-          .map(line => line.split(',')) // カンマの削除
-          .filter(record => record[1].trim() === "India") // 指定したものだけ取り出す。
-          .map(record => ({city: record[0].trim(), phone: record[2].trim()})) // 出力の形式に設定。
-          ;
-  return result;
+  return lines
+        .slice(1)
+        .filter(line => line.trim() !== '')
+        .map(line => line.split(','))
+        .filter(fields => fields[1].trim() === "India")
+        .map(fields => ({city: fields[0].trim(), phone: fields[2].trim()}))
+        ;
 }
