ebf776530601ffebf95e55822b70aa6b4b31066e
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-05 14:04:52 +0900

    アサーションの導入

--- src/chapter08/moveField1.js
+++ src/chapter08/moveField1.js
@@ -2,13 +2,17 @@ export class Account {
   constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    // assert(interestRate === this._type.interestRate);
     this._interestRate = interestRate;
   }
-  get interestRate() {return this._interestRate;}
+  get interestRate() { return this._interestRate; }
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
   }
+
+  get interestRate() { return _interestRate; }
 }
