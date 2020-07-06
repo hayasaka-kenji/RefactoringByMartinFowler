1588b8047dd02a50ea348188c30061eada9fde59
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-06 19:16:01 +0900

    条件記述の統合。

--- src/chapter10/replaceNestedConditionalWithGuardClauses1.js
+++ src/chapter10/replaceNestedConditionalWithGuardClauses1.js
@@ -1,8 +1,7 @@
 // 条件記述を逆にして、「ガード節による入れ子の置き換え」
 export function adjustedCapital(anInstrument) {
-  let result = 0;
-  if (anInstrument.capital <= 0) return result;
-  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
-  result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-  return result;
+  if (anInstrument.capital <= 0
+    || anInstrument.interestRate <= 0
+    || anInstrument.duration <= 0) return 0;
+  return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
