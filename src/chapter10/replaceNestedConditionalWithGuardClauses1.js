21b5985d77952340c28450d92251ed7eb42c26ab
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-06 19:07:59 +0900

    最初の条件判定を逆にする。

--- src/chapter10/replaceNestedConditionalWithGuardClauses1.js
+++ src/chapter10/replaceNestedConditionalWithGuardClauses1.js
@@ -1,10 +1,9 @@
 // 条件記述を逆にして、「ガード節による入れ子の置き換え」
 export function adjustedCapital(anInstrument) {
   let result = 0;
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    }
+  if (anInstrument.capital <= 0) return result;
+  if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
   }
   return result;
 }
